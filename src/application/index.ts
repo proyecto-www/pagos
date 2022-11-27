e38c36b525463c3ba87eb400d866e6eedd73433a
David Velasco <velasco.david@correounivalle.edu.co>
2022-11-26 23:18:13 -0500

    Corrige error en serverless.yml

--- src/application/index.ts
+++ src/application/index.ts
@@ -5,5 +5,6 @@ import ResponseCustom from '../infrastructure/controller/response/ResponseCustom
 
 export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResult> => {
 
+    console.log(JSON.stringify(event,null,2))
     return new ResponseCustom(400,'No hay metodos configurados')
 }
